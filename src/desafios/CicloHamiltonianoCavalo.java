f1737df39934cd5e8653c4202408db536abd49cd
Lucas Rodrigues <cercaoc@gmail.com>
2016-04-05 23:54:07 -0300

    Alterando para hashtable

--- src/desafios/CicloHamiltonianoCavalo.java
+++ src/desafios/CicloHamiltonianoCavalo.java
@@ -30,181 +30,6 @@ public class CicloHamiltonianoCavalo {
 	Hashtable vertices = new Hashtable<>();
 	List<Object> verticesProibidos = new ArrayList<Object>();
 
-	public void buscarCicloHamiltoniano_old(int[][] grafo) {	
-		
-		System.out.println((new Date()).toString() + ": " +  String.format( "%.2f", totalCont ));
-		TimerTask hourlyTask = new TimerTask () {
-		    @Override
-		    public void run () {
-		    	System.out.println((new Date()).toString() + ": " +  String.format( "%.2f", totalCont )
-		    	+
-		    	"; Velocidade: " + String.format( "%.2f", totalCont - valorAnterior) + "it/h; "
-		    	+ "Caminho: " + convertCaminhoToString(caminho));
-		    	
-		    	if (verticesVizinhos != null)
-		    		System.out.println(verticesVizinhos.length);
-		    	
-		    	valorAnterior = totalCont;
-		    }
-		};
-
-		// agenda timer para executar a cada uma hora
-		timer.schedule (hourlyTask, 0l, 1000*60*60);
-		
-		matrizGrafo = grafo;
-		
-		 // inicializa tamanho  
-		caminho = new int[matrizGrafo.length];
-
-		// Preenche o caminho com -1
-		Arrays.fill(caminho, -1);
-		
-		try {
-			// Inicializa o caminho com 0: é rótulo para o primeiro vértice do caminho
-			caminho[0] = 0;
-			
-			// Inicializa o contator
-			caminhoCount = 1;
-			
-			// Inicia a funcao recursiva de busca passando como origem o vertice 0
-			buscar(0);
-			System.out.println("Não existe caminho hamiltoniano no grafo avaliado. Total Iteracoes: " + totalCont);
-		} catch (Exception e) {
-			
-			// para o timer
-			timer.cancel();
-			
-			// se passou uma exceção, mostra o caminho:			
-			System.out.print("\nCiclo Hamiltoniano encontrado: ");
-			for (int i = 0; i <= caminho.length; i++)
-				System.out.print(caminho[i] + " ");		
-			
-			System.out.println(" Total Iteracoes: " + String.format( "%.2f", totalCont ));
-		}
-	}
-
-	public void buscar_old(int vertice) throws Exception {
-		
-		// verifica se o vértice atual está conectado com o primeiro e se é o último grafo
-		if (matrizGrafo[vertice][0] == 1 && caminhoCount == matrizGrafo.length)
-			throw new Exception("Caminho: ");
-		
-		// se é o último e não encontrou, para de buscar e mostra a mensagem
-		if (caminhoCount == matrizGrafo.length)
-			return;
-
-		// No algoritmo do caminho do cavalo é necessário avaliar apenas os vizinho (que contém o vértice conectado) ao invés
-		// de iterar todos os vértices		
-		
-		// Encontra vizinhos
-		int[] verticesVizinhos = this.getVizinhos(vertice);
-		
-		// Itera os vértices vizinhos 		
-		for (int v = 0; v < verticesVizinhos.length; v++) {
-						
-			// Contabiliza
-			totalCont++;
-			if(getTamanhoCaminho(caminho) > matrizGrafo.length - 3){
-		    	System.out.println((new Date()).toString() + ": " +  String.format( "%.2f", totalCont )
-		    	+
-		    	"; QUASE: Velocidade: " + String.format( "%.2f", totalCont - valorAnterior) + "it/h; "
-		    	+ "Caminho: " + convertCaminhoToString(caminho));
-		    	
-		    	if (verticesVizinhos != null)
-		    		System.out.println(verticesVizinhos.length);
-			}				
-			
-			// não precisa verificar se já é conectado por que estamos iterando apenas vizinhos
-				
-			// adiciono no caminho e incremento o contador
-			caminho[caminhoCount++] = verticesVizinhos[v];
-			
-			// Remove a conexao
-			matrizGrafo[vertice][verticesVizinhos[v]] = 0;
-			matrizGrafo[verticesVizinhos[v]][vertice] = 0;
-
-			// se o vértice não foi visitado, busca novamente
-			if (!foiSelecionado(verticesVizinhos[v]))
-				buscar(verticesVizinhos[v]);
-
-			// restaura conexao se já foi visitado
-			matrizGrafo[vertice][verticesVizinhos[v]] = 1;
-			matrizGrafo[verticesVizinhos[v]][vertice] = 1;
-			
-			// remove do caminho e remove 1 do contador
-			caminho[--caminhoCount] = -1;
-			
-		}
-	}
-	
-	public void addVertex(Object v) throws Exception {
-        //if (vertices.containsKey(v))
-        //    throw new Exception("Este vértice já existe");
-        vertices.put(v,new Hashtable());
-    }
-	
-	public void connect(Object v1, Object v2) throws Exception {
-        //if (!vertices.containsKey(v1))
-//            throw new Exception("Elemento não encontrado");
-//        if (!vertices.containsKey(v2))
-//            throw new Exception("Elemento não encontrado");
-        if (v1==v2)
-            return;
-        ((Hashtable)vertices.get(v1)).put(v2,v2);
-        ((Hashtable)vertices.get(v2)).put(v1,v1);
-    }
-	
-	public Object[] getNeighboorsArray(Object v) throws Exception {
-        //if (!vertices.containsKey(v))
-        //	throw new Exception("Elemento não encontrado");
-        Object[] neighboors = new Object[degree(v)];
-        int i=0;
-        for (Enumeration e=getNeighboors(v); e.hasMoreElements(); i++)
-            neighboors[i]=e.nextElement();
-        return neighboors;
-    }
-	
-	 public void disconnect(Object v1, Object v2) throws Exception {
-	        //if (!vertices.containsKey(v1))
-	        //	throw new Exception("Elemento não encontrado");
-	        //if (!vertices.containsKey(v2))
-	        //	throw new Exception("Elemento não encontrado");
-	        if (v1==v2)
-	            return;
-	        
-	        ((Hashtable)vertices.get(v1)).remove(v2);
-	        ((Hashtable)vertices.get(v2)).remove(v1);
-	    }
-	
-	public int degree(Object v) throws Exception {
-        //if (!vertices.containsKey(v))
-        //	throw new Exception("Elemento não encontrado");
-        return ((Hashtable)vertices.get(v)).size();
-    }
-	
-	public int size() {
-        return vertices.size();
-    }
-	public Enumeration getVertices() {
-        return vertices.keys();
-    }
-	
-	public Enumeration getNeighboors(Object v) throws Exception {
-        //if (!vertices.containsKey(v))
-        //	throw new Exception("Elemento não encontrado");
-        return ((Hashtable)vertices.get(v)).elements();
-    }
-
-	public boolean isConnected(Object v1, Object v2) throws Exception {
-//        if (!vertices.containsKey(v1))
-//        	throw new Exception("Elemento não encontrado");
-//        if (!vertices.containsKey(v2))
-//        	throw new Exception("Elemento não encontrado");
-        
-        // vizinhos de v1
-        Hashtable neighboors = (Hashtable) vertices.get(v1); 
-        return (neighboors.containsKey(v2));
-    }
 	
 	public void buscarCicloHamiltoniano(int[][] grafo) {	
 		
@@ -398,4 +223,74 @@ public class CicloHamiltonianoCavalo {
 				return true;
 		return false;
 	}
+	
+	public void addVertex(Object v) throws Exception {
+        //if (vertices.containsKey(v))
+        //    throw new Exception("Este vértice já existe");
+        vertices.put(v,new Hashtable());
+    }
+	
+	public void connect(Object v1, Object v2) throws Exception {
+        //if (!vertices.containsKey(v1))
+//            throw new Exception("Elemento não encontrado");
+//        if (!vertices.containsKey(v2))
+//            throw new Exception("Elemento não encontrado");
+        if (v1==v2)
+            return;
+        ((Hashtable)vertices.get(v1)).put(v2,v2);
+        ((Hashtable)vertices.get(v2)).put(v1,v1);
+    }
+	
+	public Object[] getNeighboorsArray(Object v) throws Exception {
+        //if (!vertices.containsKey(v))
+        //	throw new Exception("Elemento não encontrado");
+        Object[] neighboors = new Object[degree(v)];
+        int i=0;
+        for (Enumeration e=getNeighboors(v); e.hasMoreElements(); i++)
+            neighboors[i]=e.nextElement();
+        return neighboors;
+    }
+	
+	 public void disconnect(Object v1, Object v2) throws Exception {
+	        //if (!vertices.containsKey(v1))
+	        //	throw new Exception("Elemento não encontrado");
+	        //if (!vertices.containsKey(v2))
+	        //	throw new Exception("Elemento não encontrado");
+	        if (v1==v2)
+	            return;
+	        
+	        ((Hashtable)vertices.get(v1)).remove(v2);
+	        ((Hashtable)vertices.get(v2)).remove(v1);
+	    }
+	
+	public int degree(Object v) throws Exception {
+        //if (!vertices.containsKey(v))
+        //	throw new Exception("Elemento não encontrado");
+        return ((Hashtable)vertices.get(v)).size();
+    }
+	
+	public int size() {
+        return vertices.size();
+    }
+	public Enumeration getVertices() {
+        return vertices.keys();
+    }
+	
+	public Enumeration getNeighboors(Object v) throws Exception {
+        //if (!vertices.containsKey(v))
+        //	throw new Exception("Elemento não encontrado");
+        return ((Hashtable)vertices.get(v)).elements();
+    }
+
+	public boolean isConnected(Object v1, Object v2) throws Exception {
+//        if (!vertices.containsKey(v1))
+//        	throw new Exception("Elemento não encontrado");
+//        if (!vertices.containsKey(v2))
+//        	throw new Exception("Elemento não encontrado");
+        
+        // vizinhos de v1
+        Hashtable neighboors = (Hashtable) vertices.get(v1); 
+        return (neighboors.containsKey(v2));
+    }
+	
 }
